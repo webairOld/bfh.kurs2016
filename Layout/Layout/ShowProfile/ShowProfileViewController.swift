d96a14279b5981b3c9a1521be34974e2ffe7d746
Webair <chrisr.weber@gmail.com>
2016-02-27 20:18:46 +0100

    finally managed those f**** size classes

--- Layout/Layout/ShowProfile/ShowProfileViewController.swift
+++ Layout/Layout/ShowProfile/ShowProfileViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class ShowProfileViewController: UIViewController {
 
+    @IBOutlet var profileImage: UIImageView!
+    @IBOutlet var name: UILabel!
+    @IBOutlet var phone: UILabel!
+    @IBOutlet var email: UILabel!
+    @IBOutlet var about: UITextView!
+    
     var profile:ProfileViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.profileImage.image = profile.image
+        self.name.text = profile.name
+        self.phone.text = profile.phoneNumber
+        self.email.text = profile.email
+        self.about.text = profile.about
     }
 }
