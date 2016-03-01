249167e2e325591c0c05de5670dac280e2f99f10
Webair <chrisr.weber@gmail.com>
2016-03-01 18:07:28 +0100

    implemented inmemory store

---
+++ SwiftDiver/SwiftDiver/ListBuzzwords/ListBuzzwordsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ListBuzzwordsViewController.swift
+//  SwiftDiver
+//
+//  Created by Chris Weber on 01.03.16.
+//  Copyright © 2016 BFH. All rights reserved.
+//
+
+import Cocoa
+
+class ListBuzzwordsViewController: UIViewController {
+
+}
---
+++ SwiftDiver/SwiftDiver/Models/Buzzword.swift
@@ -0,0 +1,9 @@
+//
+//  Buzzword.swift
+//  SwiftDiver
+//
+//  Created by Chris Weber on 01.03.16.
+//  Copyright © 2016 BFH. All rights reserved.
+//
+
+import Foundation
