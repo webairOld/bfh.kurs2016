dfcdb08da54a4ff915537682eb769d8d93256fa7
Webair <chrisr.weber@gmail.com>
2016-03-01 18:14:04 +0100

    updated exercise

--- SwiftDiver/SwiftDiverTests/DataStore/InMemoryBuzzwordStoreTest.swift
+++ SwiftDiver/SwiftDiverTests/DataStore/InMemoryBuzzwordStoreTest.swift
@@ -14,35 +14,35 @@ import SwiftDiver
 class InMemoryBuzzwordStoreTest: XCTestCase {
     let sut = InMemoryBuzzwordStore()
     
-    func testCreateBuzzword() {
-        sut.createBuzzword("HTML5")
-        
-        XCTAssertEqual(sut.allBuzzwords().count, 1)
-    }
-    
-    func testCreateBuzzword_InitialValues() {
-        sut.createBuzzword("HTML 5")
-        
-        XCTAssertEqual(sut.allBuzzwords()[0].id, 1)
-        XCTAssertEqual(sut.allBuzzwords()[0].name, "HTML 5")
-        XCTAssertEqual(sut.allBuzzwords()[0].count, 0)
-    }
-    
-    func testCreateBuzzword_IncrementIds() {
-        sut.createBuzzword("HTML 5")
-        sut.createBuzzword("Responsive Design")
-        
-        XCTAssertEqual(sut.allBuzzwords()[0].id, 1)
-        XCTAssertEqual(sut.allBuzzwords()[1].id, 2)
-    }
-    
-    func testSaveBuzzword_IncrementCount() {
-        sut.createBuzzword("HTML 5")
-        let incrementedBuzzword = sut.allBuzzwords()[0].incrementCount()
-        
-        sut.saveBuzzword(incrementedBuzzword)
-        
-        XCTAssertEqual(sut.allBuzzwords()[0].count, 1)
-    }
+//    func testCreateBuzzword() {
+//        sut.createBuzzword("HTML5")
+//        
+//        XCTAssertEqual(sut.allBuzzwords().count, 1)
+//    }
+//    
+//    func testCreateBuzzword_InitialValues() {
+//        sut.createBuzzword("HTML 5")
+//        
+//        XCTAssertEqual(sut.allBuzzwords()[0].id, 1)
+//        XCTAssertEqual(sut.allBuzzwords()[0].name, "HTML 5")
+//        XCTAssertEqual(sut.allBuzzwords()[0].count, 0)
+//    }
+//    
+//    func testCreateBuzzword_IncrementIds() {
+//        sut.createBuzzword("HTML 5")
+//        sut.createBuzzword("Responsive Design")
+//        
+//        XCTAssertEqual(sut.allBuzzwords()[0].id, 1)
+//        XCTAssertEqual(sut.allBuzzwords()[1].id, 2)
+//    }
+//    
+//    func testSaveBuzzword_IncrementCount() {
+//        sut.createBuzzword("HTML 5")
+//        let incrementedBuzzword = sut.allBuzzwords()[0].incrementCount()
+//        
+//        sut.saveBuzzword(incrementedBuzzword)
+//        
+//        XCTAssertEqual(sut.allBuzzwords()[0].count, 1)
+//    }
     
 }
